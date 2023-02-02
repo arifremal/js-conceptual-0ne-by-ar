739f0e321ba4689cba2f814cac7030f73781f3d2
arifremal <arifremal013@gmail.com>
2023-02-02 20:43:59 +0600

    geade system done

--- app.js
+++ app.js
@@ -20,8 +20,7 @@
 // let num3= 10;
 // if (num1 == 30 && num2 == 89 || num3==1) {
 //     console.log('yes');
-    
-    
+
 // } else if (num1 == 1 && num2 == 4){
 //     console.log('noooo');
 // }
@@ -29,4 +28,49 @@
 //     console.log('kicu e na');
 // }
 
-let 
+// let number ="30";
+// if ( number == 30 ) {
+//     if (typeof  number =='string' )
+//     {
+//         console.log('okay ');
+
+//     }
+
+// }
+
+// let num = 100;
+// if (num % 2 == 1){
+//     console.log('odd');
+// }
+// else{
+//     console.log('even');
+// }
+// Grade i
+// 0 to 32 f
+// 33 to 49 b
+// 50 t0 59 c
+// 60 to 69 b
+// 70 t0 79 a
+// 80 to 100 A+
+// let result = 85;
+// if (result > 100) {
+//   console.log("kemne ki bhai ");
+// } else if (result <= 100 && result >= 0) {
+//   if (result >= 0 && result < 33) {
+//     console.log("F");
+//   } else if (result > 32 && result < 50) {
+//     console.log("D");
+//   }
+//  else if ( result > 51 && result <69){
+//     console.log('B');
+//  }
+//  else if ( result > 70 && result <81){
+//     console.log('A');
+//  }
+//  else if ( result > 81 && result <90){
+//     console.log('A+');
+//  }
+  
+// } else {
+//   console.log("0 teke o kom");
+// }
