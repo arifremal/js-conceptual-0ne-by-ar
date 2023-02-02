9030adc8c732e584b678ce6e1fa3c72e3cc48b8f
arifremal <arifremal013@gmail.com>
2023-02-02 19:36:32 +0600

    if start

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var nam = 'rahim';
-console.log( typeof true );
+// var nam = 'rahim';
+// console.log( typeof true );
+
+let nam = '90.98' ;
+console.log( parseFloat(nam));
+
+// if else start here
+
