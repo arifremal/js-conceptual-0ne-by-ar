ce6cce5d15e19bb4d038d232719046df9a7c2eb5
arifremal <arifremal013@gmail.com>
2023-02-02 20:05:01 +0600

    nested start

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 // var nam = 'rahim';
 // console.log( typeof true );
 
-let nam = '90.98' ;
-console.log( parseFloat(nam));
+// let nam = '90.98' ;
+// console.log( parseFloat(nam));
 
 // if else start here
+// var first  = 'not';
+// if (first == 'hablu'){
+//     console.log('yes');
+// }
+// else if (first == 'no') {
+//     console.log('nope');
+// }
+// else{
+//     console.log('dhur mia');
+// }
+// let num1 =19;
+// let num2 = 4;
+// let num3= 10;
+// if (num1 == 30 && num2 == 89 || num3==1) {
+//     console.log('yes');
+    
+    
+// } else if (num1 == 1 && num2 == 4){
+//     console.log('noooo');
+// }
+// else{
+//     console.log('kicu e na');
+// }
 
+let 
